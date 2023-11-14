52ee686e26cb5f0978e24c583a6a31b50321ab3a
JB van der Merwe <jb@jbkamp.com>
2023-11-14 12:08:47 +0200

    Refactor grid_view.

--- lib/views/grid_view.dart
+++ lib/views/grid_view.dart
@@ -19,7 +19,7 @@ class GridGraph extends StatelessWidget {
           // Margin around the container.
           margin: const EdgeInsets.all(50.0),
           // CustomPaint widget used for drawing custom designs.
-          color: Color.fromRGBO(22, 72, 99, 1),
+          color: const Color.fromRGBO(22, 72, 99, 1),
           child: CustomPaint(
             // Setting size of the CustomPaint based on parent constraints.
             size: Size(constraints.maxWidth, constraints.maxHeight),
@@ -40,7 +40,7 @@ class _GridPainter extends CustomPainter {
   @override
   void paint(Canvas canvas, Size size) {
     final paint     = Paint()
-      ..color       = Color.fromRGBO(66, 125, 157, 1)
+      ..color       = const Color.fromRGBO(66, 125, 157, 1)
       ..strokeWidth = 0.5;
 
     final stepX = size.width / 100;
@@ -53,19 +53,19 @@ class _GridPainter extends CustomPainter {
       canvas.drawLine(Offset(i * stepX, 0), Offset(i * stepX, size.height), paint);
     }
 
-    final blackPaint  = Paint()
-      ..color         = Color.fromRGBO(66, 125, 157, 1)
+    final gridLinePaintPaint  = Paint()
+      ..color         = const Color.fromRGBO(66, 125, 157, 1)
       ..strokeWidth   = 1.0;
 
     for (double i = 0; i <= 100; i += 10) {
       // Draw horizontal grid lines
-      canvas.drawLine(Offset(0, size.height - i * stepY), Offset(size.width, size.height - i * stepY), blackPaint);
+      canvas.drawLine(Offset(0, size.height - i * stepY), Offset(size.width, size.height - i * stepY), gridLinePaintPaint);
       // Draw vertical grid lines
-      canvas.drawLine(Offset(i * stepX, 0), Offset(i * stepX, size.height), blackPaint);
+      canvas.drawLine(Offset(i * stepX, 0), Offset(i * stepX, size.height), gridLinePaintPaint);
     }
 
-    final redPaint =   Paint()
-      ..color       = Color.fromRGBO(155, 190, 200, 1)
+    final coordinatePathPaint =   Paint()
+      ..color       = const Color.fromRGBO(155, 190, 200, 1)
       ..strokeWidth = 2.0
       ..style       = PaintingStyle.stroke;
 
@@ -77,17 +77,26 @@ class _GridPainter extends CustomPainter {
     }
 
     path.close();
-    canvas.drawPath(path, redPaint);
+    canvas.drawPath(path, coordinatePathPaint);
 
+    _drawCoordinateLabels(canvas, size, points, stepX, stepY);
+
+    _drawAxisLabels(canvas, size, stepX, stepY);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) => true;
+
+  void _drawCoordinateLabels(Canvas canvas, Size size, List<List<double>> coordinates, double stepX, double stepY) {
     final textPainter = TextPainter(
       textDirection: TextDirection.ltr,
     );
 
-    for (var point in points) {
+    for (var coordinate in coordinates) {
       // Create a TextSpan with larger font size, different text and background color.
       textPainter.text = TextSpan(
-        text: '(${point[0]},${point[1]})',
-        style: TextStyle(
+        text: '(${coordinate[0]},${coordinate[1]})',
+        style: const TextStyle(
           color: Color.fromRGBO(22, 72, 99, 1), // Change text color here
           fontSize: 12.0, // Increase font size
           backgroundColor: Color.fromRGBO(221, 242, 253, 1), // Change background color here
@@ -98,7 +107,7 @@ class _GridPainter extends CustomPainter {
       textPainter.layout();
 
       // Calculate the position for the text.
-      Offset textPosition = Offset(point[0] * stepX - 10, size.height - point[1] * stepY - textPainter.height - 10);
+      Offset textPosition = Offset(coordinate[0] * stepX - 10, size.height - coordinate[1] * stepY - textPainter.height - 10);
 
       // Calculate the rectangle bounds for the background.
       Rect backgroundRect = Rect.fromLTWH(
@@ -114,40 +123,40 @@ class _GridPainter extends CustomPainter {
       // Draw the rounded background rectangle.
       canvas.drawRRect(
         RRect.fromRectXY(backgroundRect, cornerRadius, cornerRadius),
-        Paint()..color = Color.fromRGBO(221, 242, 253, 1),
+        Paint()..color = const Color.fromRGBO(221, 242, 253, 1),
       );
 
       // Draw the text.
       textPainter.paint(canvas, textPosition);
     }
+  }
 
+  void _drawAxisLabels(Canvas canvas, Size size, double stepX, double stepY)
+  {
     final axisTextPainter = TextPainter(
       textDirection: TextDirection.ltr,
     );
 
-// Draw labels for horizontal axis steps
-for (double i = 0; i <= 100; i += 10) {
-  axisTextPainter.text = TextSpan(
-    text: i.toString(),
-    style: const TextStyle(color: Colors.black, fontSize: 12.0),
-  );
+    // Draw labels for horizontal axis steps
+    for (double i = 0; i <= 100; i += 10) {
+      axisTextPainter.text = TextSpan(
+        text: i.toString(),
+        style: const TextStyle(color: Colors.black, fontSize: 12.0),
+      );
 
-  axisTextPainter.layout();
-  axisTextPainter.paint(canvas, Offset(i * stepX, size.height - axisTextPainter.height + 20));
-}
+      axisTextPainter.layout();
+      axisTextPainter.paint(canvas, Offset(i * stepX, size.height - axisTextPainter.height + 20));
+    }
 
-// Draw labels for vertical axis steps
-for (double i = 0; i <= 100; i += 10) {
-  axisTextPainter.text = TextSpan(
-    text: i.toString(),
-    style: const TextStyle(color: Colors.black, fontSize: 12.0),
-  );
+    // Draw labels for vertical axis steps
+    for (double i = 0; i <= 100; i += 10) {
+      axisTextPainter.text = TextSpan(
+        text: i.toString(),
+        style: const TextStyle(color: Colors.black, fontSize: 12.0),
+      );
 
-  axisTextPainter.layout();
-  axisTextPainter.paint(canvas, Offset(-20, size.height - i * stepY - axisTextPainter.height));
-}
+      axisTextPainter.layout();
+      axisTextPainter.paint(canvas, Offset(-20, size.height - i * stepY - axisTextPainter.height));
+    }
   }
-
-  @override
-  bool shouldRepaint(CustomPainter oldDelegate) => true;
 }
